39312e14cd74a3de8fdc74b07226dcda5bf99ce8
manikantakailasa <manikanta.kailasa@gmail.com>
2024-01-08 16:27:59 +0530

    facebook connect settings page

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-element'), 'version' => '315063ba5c521ecb828e');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-element'), 'version' => '9de3c7d1ecfaad5a3969');
