116754f0db66092d58bfc7c97b9c49dec7cc2343
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-02-12 11:11:40 +0530

    changes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '8ff8f172454392d65453');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '309d0fcca68ff6a9a967');
