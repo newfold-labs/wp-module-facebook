67009c39ef7b271ca281501eb51ce4ad11b59771
Evan Mullins <evanrm@gmail.com>
2025-01-24 11:32:47 -0500

    release prep for `1.1.2`

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '8290289d821b05f79d1f');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'b0d09dd6852a1937b0e7');
